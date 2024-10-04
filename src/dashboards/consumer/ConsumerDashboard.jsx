8a49e634e8d52753c7f448a996b242848e271c07
Andrew Archer <a.archer@erasolutions.us>
2024-10-04 00:41:55 -0400

    feature: integrated account drawer api functionality (partly)

--- src/dashboards/consumer/ConsumerDashboard.jsx
+++ src/dashboards/consumer/ConsumerDashboard.jsx
@@ -7,25 +7,103 @@ import {
     Divider,
     Drawer,
     Grid2,
+    Input,
     List,
     ListItem,
     Typography,
 } from "@mui/material";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { useSelector } from "react-redux";
+import Modal from "@mui/material/Modal";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import bcrypt from "bcryptjs";
+import { updateUserInfo } from "../../features/user/userSlice";
 
 function ConsumerDashboard() {
+    const dispatch = useDispatch();
     const [state, setState] = useState({
         right: false,
     });
-    const {company_name, f_name, l_name} = useSelector((state) => state.user.data)
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [infoModal, setInfoModal] = useState(false);
+    const handleOpenInfo = () => setInfoModal(true);
+    const handleCloseInfo = () => setInfoModal(false);
+    const { company_name, f_name, l_name, email, phone, id, token } =
+        useSelector((state) => state.user.data);
+    const { type } = useSelector((state) => state.user);
 
-    // TODO: Write logic for sending axios call to send order
+    //State to handle updating user info form
+    const [firstName, setFirstName] = useState(f_name);
+    const handleFirstNameChange = (e) => setFirstName(e.target.value);
+    const [lastName, setLastName] = useState(l_name);
+    const handleLastNameChange = (e) => setLastName(e.target.value);
+    const [userEmail, setEmail] = useState(email);
+    const handleEmailChange = (e) => setEmail(e.target.value);
+    const [userPhone, setPhone] = useState(phone);
+    const handlePhoneChange = (e) => setPhone(e.target.value);
+    const [userComapany, setUserCompany] = useState(company_name);
+    const handleCompanyChange = (e) => setUserCompany(e.target.value);
+
+    async function handleUpdateInfo(e) {
+        e.preventDefault();
+        const data = {
+            f_name: e.target[0].value,
+            l_name: e.target[1].value,
+            email: e.target[2].value,
+            phone: e.target[3].value,
+            company_name: e.target[4].value,
+            token: token,
+            user_type: type,
+            userID: id
+        }
+        await dispatch(updateUserInfo(data)).then(function () {
+            alert("User info updated succesfully");
+            handleCloseInfo();
+        });
+    }
 
     const toggleDrawer = (anchor, open, toggle) => (event) => {
         if ((event.type === "keydown" && event.key === "Esc") || toggle)
             setState({ ...state, [anchor]: open });
     };
+
+    async function handlePasswordChange(e) {
+        e.preventDefault();
+        const oldPass = e.target[0].value;
+        let newPass = e.target[1].value;
+        const config = {
+            headers: { Authorization: token },
+        };
+
+        try {
+            const url = `http://localhost:8080/account/getStoredPass/${type}/${id}`;
+            let validPass = await axios.get(url, config);
+
+            if (bcrypt.compareSync(oldPass, validPass.data[0].password)) {
+                const url = `http://localhost:8080/account/updatePassword`;
+                const salt = bcrypt.genSaltSync(10);
+                newPass = bcrypt.hashSync(newPass, salt);
+                let data = {
+                    user_type: type,
+                    userID: id,
+                    newPass: newPass,
+                };
+
+                await axios.put(url, data, config).then(function (res) {
+                    alert("Password changed sucsesfully");
+                    handleClose();
+                });
+            } else {
+                alert("Old password does not match current stored password");
+                return;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     // TODO: Pass in data for fields (product name, individual/unit price, numPerUnit, NumUnitsAvail, image)
     const accountDrawer = (anchor) => (
@@ -45,11 +123,161 @@ function ConsumerDashboard() {
             </Typography>
             <List>
                 <ListItem>
-                    <Typography variant="h4">Welcome {f_name} {l_name}</Typography>
+                    <Typography variant="h4">
+                        {f_name} {l_name}
+                    </Typography>
                 </ListItem>
                 <Divider />
                 <ListItem>
-                    <Typography>Account Info</Typography>
+                    <Grid2 container rowSpacing={1}>
+                        <Grid2 item size={6}>
+                            <Typography variant="h6">Email:</Typography>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Typography>{email}</Typography>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Typography variant="h6">Phone:</Typography>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Typography>{phone}</Typography>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Typography variant="h6">Company Name</Typography>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Typography>{company_name}</Typography>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Button fullWidth onClick={handleOpen}>
+                                Change Password
+                            </Button>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                                className={styles.passWordModel}
+                            >
+                                <Box className={styles.passWordModelBox}>
+                                    <form
+                                        className={styles.passform}
+                                        onSubmit={(e) =>
+                                            handlePasswordChange(e)
+                                        }
+                                    >
+                                        <Input
+                                            name="oldPass"
+                                            color="white"
+                                            type="password"
+                                            placeholder="Enter old password..."
+                                            fullWidth
+                                            className={styles.passwordInput}
+                                        />
+                                        <Input
+                                            name="newPass"
+                                            type="password"
+                                            placeholder="Enter new password..."
+                                            fullWidth
+                                            className={styles.passwordInput}
+                                        />
+                                        <Button type="submit">Update</Button>
+                                    </form>
+                                </Box>
+                            </Modal>
+                        </Grid2>
+                        <Grid2 item size={6}>
+                            <Modal
+                                open={infoModal}
+                                onClose={handleCloseInfo}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                                className={styles.passWordModel}
+                            >
+                                <Box className={styles.infoModal}>
+                                    <form onSubmit={(e) => handleUpdateInfo(e)}>
+                                        <Grid2 container rowSpacing={1}>
+                                            <Grid2 item size={6}>
+                                                <Typography variant="h6">
+                                                    First Name:
+                                                </Typography>
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Input
+                                                    fullWidth
+                                                    type="text"
+                                                    value={firstName}
+                                                    onChange={handleFirstNameChange}
+                                                />
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Typography variant="h6">
+                                                    Last Name:
+                                                </Typography>
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Input
+                                                    fullWidth
+                                                    type="text"
+                                                    value={lastName}
+                                                    onChange={handleLastNameChange}
+                                                />
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Typography variant="h6">
+                                                    Email:
+                                                </Typography>
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Input
+                                                    fullWidth
+                                                    type="text"
+                                                    value={userEmail}
+                                                    onChange={handleEmailChange}
+                                                />
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Typography variant="h6">
+                                                    Phone:
+                                                </Typography>
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Input
+                                                    fullWidth
+                                                    type="text"
+                                                    value={userPhone}
+                                                    onChange={handlePhoneChange}
+                                                />
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Typography variant="h6">
+                                                    Company Name
+                                                </Typography>
+                                            </Grid2>
+                                            <Grid2 item size={6}>
+                                                <Input
+                                                    fullWidth
+                                                    type="text"
+                                                    value={userComapany}
+                                                    onChange={
+                                                        handleCompanyChange
+                                                    }
+                                                />
+                                            </Grid2>
+                                            <Grid2 item size={12}>
+                                                <Button type="submit" fullWidth>
+                                                    Confirm Changes
+                                                </Button>
+                                            </Grid2>
+                                        </Grid2>
+                                    </form>
+                                </Box>
+                            </Modal>
+                            <Button fullWidth onClick={handleOpenInfo}>
+                                Update User Info
+                            </Button>
+                        </Grid2>
+                    </Grid2>
                 </ListItem>
                 <Divider />
                 <ListItem>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -4,14 +4,42 @@ import axios from "axios";
 export const signInUser = createAsyncThunk(
     "user/signInUser",
     async (data, thunkAPI) => {
-        try{
-            const url = 'http://localhost:8080/auth/signIn';
+        try {
+            const url = "http://localhost:8080/auth/signIn";
             let response = await axios.post(url, data);
-            response = {data: response.data, type: data.user_type};
+            response = { data: response.data, type: data.user_type };
             return response;
         } catch (error) {
-            if(error.status === 401) alert('Wrong email or password')
-            return thunkAPI.rejectWithValue({error: error.message});
+            if (error.status === 401) alert("Wrong email or password");
+            return thunkAPI.rejectWithValue({ error: error.message });
+        }
+    }
+);
+
+export const updateUserInfo = createAsyncThunk(
+    "user/updateUserInfo",
+    async (data, thunkAPI) => {
+        const { f_name, l_name, email, phone, company_name, user_type, token, userID } = data;
+        try {
+            const url = "http://localhost:8080/account/updateUserInfo";
+            const config = {
+                headers: {
+                    Authorization: token,
+                },
+            };
+            const body = {
+                f_name: f_name,
+                l_name: l_name,
+                email: email,
+                phone: phone,
+                company_name: company_name,
+                user_type: user_type,
+                userID: userID
+            };
+            let response = await axios.put(url, body, config);
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue({ error: error.message });
         }
     }
 );
@@ -28,23 +56,40 @@ const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {},
-    extraReducers: builder => {
+    extraReducers: (builder) => {
         builder
-        .addCase(signInUser.pending, state => {
-            state.loading = true;
-            state.error = null;
-        })
-        .addCase(signInUser.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = null;
-            state.data = action.payload.data;
-            state.type = action.payload.type;
-        })
-        .addCase(signInUser.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message;
-        })
-    }
+            .addCase(signInUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(signInUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = null;
+                state.data = action.payload.data;
+                state.type = action.payload.type;
+            })
+            .addCase(signInUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+            .addCase(updateUserInfo.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(updateUserInfo.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = null;
+                state.data.f_name = action.payload.f_name;
+                state.data.l_name = action.payload.l_name;
+                state.data.email = action.payload.email;
+                state.data.phone = action.payload.phone;
+                state.data.company_name = action.payload.company_name;
+            })
+            .addCase(updateUserInfo.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            });
+    },
 });
 
 export const {} = userSlice.actions;
